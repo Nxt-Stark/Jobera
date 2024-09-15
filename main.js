0ec94e3bf0980f348d75d37e3a3ae10b7843836f
Nxt-Stark <hadilkk63@gmail.com>
2024-09-15 11:01:16 +0530

    snd

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 window.addEventListener('load', function() {
-    setTimeout(function() {
-      document.body.classList.add('loaded');
-    }, 0);
-  });
+  setTimeout(function() {
+      document.getElementById('loader-container').style.display = 'none';
+  }, 3000);
+});
 
+
+
+  const mybutton = document.getElementById("btn-back-to-top");
+
+  window.onscroll = () => {
+    mybutton.style.display = (window.scrollY > 20) ? "block" : "none";
+  };
+  
+  mybutton.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+  
+
+function subscribe(){
+  window.alert("Succesfully Subscribed!!")
+}
